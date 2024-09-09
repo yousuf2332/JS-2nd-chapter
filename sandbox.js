d1164fd77c9b9164fa97af63212a6d8a44667bcc
yousuf2332 <yousuf23325268@gmail.com>
2024-09-09 10:59:53 -0700

    Add files via upload

---
+++ sandbox.js
@@ -0,0 +1,27 @@
+var myName = "Abdullah Aslam";
+console.log(myName);
+
+var message = "HELLO WORLD";
+alert(message);
+
+var userName = "Jhone Doe";
+var age = "15 years old";
+var course = "Certified Mobile Application Development";
+
+alert(userName);
+alert(age);
+alert(course);
+
+var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert(pizza);
+
+var email = "My email address is ";
+alert(email + "aabdullahaslam91@gmail.com");
+
+var Book = "A smarter way to learn Javascript";
+alert("I am trying to learn from the Book. " + Book);
+
+document.getElementById("demo").innerHTML = "Yah! I can write HTML content through Javascript";
+
+var design = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert(design);
